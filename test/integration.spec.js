f037582dccb278872ba0771bb83cf30f85fe4db1
Max Kharandziuk <max.kharandziuk@here.com>
2018-08-24 08:08:53 +0200

    handles closing event

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -22,43 +22,84 @@ const collectEvents = (eventEmmiter, eventName, count=1) => {
 const createClient = () => {
   const cDebug = debug('app:client')
   const ws = new WebSocket('ws://localhost:8080')
-  return collectEvents(ws, 'open').then(() => {
-    connect: (id) => {
-      ws.send('connect' + id)
-    },
-    ws
-  })
+  return collectEvents(ws, 'open').then(() => ws)
 }
+
+let clienId = 0
+const getId = () => {
+  clienId = clienId + 1
+  return clienId - 1
+}
+
+
 
 const createServer = () => {
   const sDebug = debug('app:server')
   const server = new WebSocket.Server({ port: 8080 });
 
   server.on('connection', function connection(ws) {
+    const id = getId()
+    for (otherWs of server.clients) {
+      if (otherWs === ws) {
+        continue
+      }
+      otherWs.send(`client ${id} is connected`)
+    }
     ws.on('message', (message) => {
-      sDebug('mesage from ', message)
+      for (otherWs of server.clients) {
+        if (otherWs === ws) {
+          continue
+        }
+        otherWs.send(`client ${id} said: ${message}`)
+      }
+    })
+    ws.on('close', () => {
+      for (otherWs of server.clients) {
+        if (otherWs === ws || otherWs.readyState !== 1) {
+          continue
+        }
+        otherWs.send(`client ${id} is closed`)
+      }
     })
   });
   return server
 }
 
 let server
-before('before', function() {
+beforeEach('before', function() {
   server = createServer()
 })
 
 it('Client receive message about connected clients', async function() {
   const client1 =  await createClient()
-  await delay(100)
-  const expectation = collectEvents(client1.ws, 'message')
+  const expectation = collectEvents(client1, 'message')
   const client2 =  await createClient()
-  client2.connect()
 
   const [msg] = await expectation
-  expect(msg).eql('client is connected')
+  expect(msg).eql('client 1 is connected')
 })
 
+it('Client receive message about connected clients', async function() {
+  const client1 =  await createClient()
+  const client2 =  await createClient()
+  client1.send('something important')
+  const expectedMessages1 = collectEvents(client1, 'message')
+  const expectedMessages2 = collectEvents(client2, 'message')
+  expect(await expectedMessages1).eql(['client 3 is connected'])
+  expect(await expectedMessages2).eql(['client 2 said: something important'])
+})
 
-after('after', function() {
+it('Client receive message about closed client', async function() {
+  const client1 =  await createClient()
+  const client2 =  await createClient()
+  const expectedMessages1 = collectEvents(client1, 'message', 2)
+  client2.close()
+  expect(await expectedMessages1).eql([
+    'client 5 is connected',
+    'client 5 is closed'
+  ])
+})
+
+afterEach('after', function() {
   server.close()
 })
